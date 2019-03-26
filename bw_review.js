d8f16777eed6593a129123de75a815140d0009ee
Kenzie Baker <mbaker880@west-mec.org>
2019-03-25 18:23:41 -0700

    Begin Debugging
    I finished all code and comments and fixed the bug that was effecting the stars when hovered over, not i need to fix the timer that indicates how many stars have been selected.

--- bw_review.js
+++ bw_review.js
@@ -44,10 +44,10 @@ function init() {
       // Create a for loop to loop through the different star images
       for (var i = 0; i < stars.length; i++) {
             stars[i].style.cursor = "pointer";
-            document.addEventListener("mouseenter", lightStars);
+            stars[i].addEventListener("mouseenter", lightStars);
       }
       // Add an event listener to update specified function when requirements are met
-      document.addEventListener("keyup", updateCount);
+      document.getElementById("comment").addEventListener("keyup", updateCount);
 }
 
 // Creating a function to color the the amount of stars that the users mouse hovers over
@@ -58,16 +58,17 @@ function lightStars(e) {
       var stars = document.querySelectorAll("span#stars img");
       // Creating a loop for the stars to light up each star depending on the users mouse
       for(var i = 0; i < starNumber; i++) {
-            stars[i].src = "url(bw_star2.png)";
+            stars[i].src = "bw_star2.png";
       }
       // Creating a loop for the stars to unlight each star depending on the users mouse
       for(var i = starNumber; i < 5; i++) {
-            star[i].src = "url(bw_star.png)";
+            star[i].src = "bw_star.png";
       }
       // Changing the value of the stars based upon how many are highlighted
       document.getElementById("rating").value = starNumber + "stars";
       e.target.addEventListener("mouseleave", turnOffStars);
-      e.target.addEventListener("click", function () {
+      e.target.addEventListener("click", 
+      function () {
             e.target.removeEventListener("mouseleave", turnOffStars)
       });
 }
@@ -76,11 +77,31 @@ function lightStars(e) {
 function turnOffStars(e) {
       // Storing all of the star reviews
       var stars = document.querySelectorAll("span#stars img");
-      stars[i].src = "url(bw_star.png)";
-      document.getElementById("rating").value = " ";
+      for (var i = 0; i < 5; i++) {
+            stars[i].src = "bw_star.png";
+      }
+      document.getElementById("rating").value = "";
 } 
 
-// Create the updateCount
+// Create the updateCount function to total the number of characters typed by user into text area box
+function updateCount() {
+      // Refernces the value stored within the text area box
+      var commentText = document.getElementById("comment").value;
+      // Declares the commentText variable as the parameters in the variable labeled charCount
+      var charCount = countCharacters(commentText);
+      // References the wordCount text area box
+      var wordCount = document.getElementById("wordCount");
+      // Changing the value of wordCount to a specified value
+      wordCount.value = charCount + "/1000";
+      // If else statement for different scenarios
+      if (charCount > 1000) {
+            wordCount.style.color = "white";
+            wordCount.style.backgroundColor = "red";
+      } else {
+            wordCount.style.color = "black";
+            wordCount.style.backgroundColor = "white";
+      }
+}
 
   
   
